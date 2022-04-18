bcc104e86dbafb7c81f20e6b3c82cf49039a2968
kousalyaN <98599038+kousalyaN@users.noreply.github.com>
2022-04-18 22:59:02 +0530

    Add files via upload

---
+++ src/Assert_Equals.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Assert_Equals {
+	@Test
+	public void setup() {
+		String str = "hello";
+		assertEquals("hello",str);
+	}
+}
